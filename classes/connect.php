6aea4879db80ad20c19a0a55acb73ad83ccd494c
Teeticode <teetinormm@gmail.com>
2021-12-24 15:25:44 -0800

    final

--- classes/connect.php
+++ classes/connect.php
@@ -6,7 +6,17 @@
         private $dbname = "RGC_DASHBOARD";
         
         function connect(){
-            $connection = mysqli_connect($this->host, $this->username, $this->password, $this->dbname);
+            mysql://b22974768473db:3fbc4fe6@eu-cdbr-west-02.cleardb.net/heroku_905c59eb2484f60?reconnect=true
+            $cleardb_url = parse_url(getenv("mysql://b22974768473db:3fbc4fe6@eu-cdbr-west-02.cleardb.net/heroku_905c59eb2484f60?reconnect=true"));
+            $cleardb_server = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db = substr($cleardb_url["path"],1);
+            $active_group = 'default';
+            $query_builder = TRUE;
+            // Connect to DB
+            $connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+            
             return $connection;
         }
 
