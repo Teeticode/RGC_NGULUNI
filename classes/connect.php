54bc58852d4285c9fd32f0efb0cc25cdce1eb173
Teeticode <teetinormm@gmail.com>
2021-12-24 16:09:21 -0800

    final

--- classes/connect.php
+++ classes/connect.php
@@ -1,16 +1,14 @@
 <?php
     class Database{
-        private 
         
         function connect(){
-            $host = "eu-cdbr-west-02.cleardb.net";
-            $username = "b22974768473db";
-            $password = "3fbc4fe6";
-            $dbname = "heroku_905c59eb2484f60";
-            
-           
-            $connection = mysqli_connect($host, $username, $password, $dbname);
-            
+            $url = parse_url(getenv("mysql://b22974768473db:3fbc4fe6@eu-cdbr-west-02.cleardb.net/heroku_905c59eb2484f60?reconnect=true"));
+            $server = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $db = substr($url["path"], 1);
+
+            $conn = mysqli_connect($server, $username, $password, $db);
             return $connection;
         }
 
