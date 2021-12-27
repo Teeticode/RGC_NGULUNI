f56491fb62f051e198b49b322b9679799599a9dc
Teeticode <teetinormm@gmail.com>
2021-12-27 12:43:15 -0800

    believe

--- index.php
+++ index.php
@@ -397,25 +397,9 @@
   <script type="text/javascript" src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script type="text/javascript" src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
   
+  
   <script type="text/javascript">
-    
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <script type="text/javascript">
-    var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 4){
-        counter = 1;
-      } 
-    },5000);
+   
     $(document).ready(function(){
          getAllUserReactions();
          setInterval(() => {
@@ -661,10 +645,20 @@
   
    
   </script>
+  <script type="text/javascript">
+    
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script type="text/javascript"  src="./assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+  <script src="./assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
 </body>
 
 </html>
